a3c52d5aa439388a89f50de22abf850573e536bc
Abhigyan Rathi <112096032+abhigyanrathi@users.noreply.github.com>
2026-09-13 19:05:57 +0530

    Bulk sync: 38 submissions

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        res = []
+        q = deque([root])
+        while q:
+            qlen = len(q)
+            for i in range(qlen):
+                node = q.popleft()
+                if i == qlen - 1:
+                    res.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return res
---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        nums.sort()
+        res = []
+        for i in range(n - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            j, k = i + 1, n - 1
+            target = -nums[i]       
+            while (j < k):
+
+                total = nums[j] + nums[k]
+                if total < target:
+                    j += 1
+                elif total > target:
+                    k -= 1
+                else:
+                    res.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                    while j < k and nums[j] == nums[j + 1]:
+                        j+= 1
+                    while j < k and nums[k] == nums[k - 1]:
+                        k -= 1
+
+            
+        return res       
